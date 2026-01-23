aae9e114f508c71270a691c180d1922373ebed57
llparagranell <paragrane000@gmail.com>
2026-01-23 12:34:06 +0530

    ec2 demo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
-import app from "./app.js";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node + PostgreSQL App");
+});
+
+app.use("/api/users", userRoutes);
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
