77dcdb725e38de994ea357c088d4ed7823103d06
udayempire <kuday0726@gmail.com>
2026-02-20 12:59:28 +0530

    feat(ui): adding navbar

---
+++ admin_frontend/src/app/(admin)/layout.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components/ui/navbar";
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
---
+++ admin_frontend/src/components/ui/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-between border-b-[1px] border-zinc-300 h-18">
+                Navbar
+            </div>            
+        </div>
+    );
+};
