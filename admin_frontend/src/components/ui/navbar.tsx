f7a65183522c27e5b9b887c2e0c8efc05e3852a5
udayempire <kuday0726@gmail.com>
2026-02-22 17:20:20 +0530

    feat: add navbar

--- admin_frontend/src/components/ui/navbar.tsx
+++ admin_frontend/src/components/ui/navbar.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
+import { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+import { LogOut } from "lucide-react";
 export const Navbar = () => {
     return (
         <div>
-            <div className="flex items-center justify-between border-b-[1px] border-zinc-300 h-18">
-                Navbar
-            </div>            
+            <div className="flex justify-between p-4  border-b-2 shadow- border-zinc-300 h-18">
+                <div className="flex items-center justify-center">
+                    <Image src="/realbroLogo2.svg" alt="real bro logo" width={140} height={100} />
+                </div>
+                
+                <div className="flex items-center justify-center gap-3">
+                    <div className="text-xl font-medium">
+                        Hi, John Doe
+                    </div>
+                    <Avatar className="h-12 w-12">
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <LogOut className="w-8 h-8 text-red-500" />
+                </div>
+            </div>
         </div>
     );
 };
